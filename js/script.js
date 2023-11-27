661dd8785d28d5079d307f554a85d308b1ebb182
Wihibaf1 <143439632+Wihibaf1@users.noreply.github.com>
2023-11-27 18:37:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollButton = document.querySelector('footer div');
+    scrollButton.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
 
